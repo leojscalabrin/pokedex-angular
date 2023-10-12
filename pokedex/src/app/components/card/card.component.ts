f497db15f62e22b425b80ed19e6f54bddeb85d1c
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-10-12 18:51:49 -0300

    integration pokeAPI

--- pokedex/src/app/components/card/card.component.ts
+++ pokedex/src/app/components/card/card.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit{
-  name:string = "VAPOREON"
-  attributesTypes:string[] = ['WATER']
+export class CardComponent implements OnInit {
+  pokemon: PokemonData;
 
-  constructor() {
-
+  constructor(private service: PokemonService) {
+    this.pokemon = {
+      id: 0,
+      name: '',
+      sprites: {
+        front_default: '',
+      },
+      types: [],
+    };
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.getPokemon('vaporeon')
   }
 
+  getPokemon(searchName:string) {
+    this.service.getPokemon(searchName).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types,
+        };
+      },
+      error: (err) => console.log('not found'),
+    });
+  }
 }
